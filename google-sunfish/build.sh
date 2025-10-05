57ec974e8d58efdfa0e59d5149a1fc6d5f4f09c5
flecke-m <matthiasfleckenstein@gmail.com>
2025-10-05 18:37:28 +0200

    update build.sh

--- google-sunfish/build.sh
+++ google-sunfish/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+[ -d build ] || git clone https://gitlab.com/ubports/porting/community-ports/halium-generic-adaptation-build-tools build
 ./build/build.sh "$@"
