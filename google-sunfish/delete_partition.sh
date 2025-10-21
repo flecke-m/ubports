c7d55f495cd523bda880ba0d96caa1837452176f
flecke-m <matthiasfleckenstein@gmail.com>
2025-10-21 19:15:00 +0200

    some updates

--- google-sunfish/delete_partition.sh
+++ google-sunfish/delete_partition.sh
@@ -2,9 +2,4 @@
 
 
 fastboot delete-logical-partition system_ext_a
-fastboot delete-logical-partition system_ext_b
 fastboot delete-logical-partition product_a
-fastboot delete-logical-partition product_b
-fastboot delete-logical-partition system_b
-fastboot delete-logical-partition vendor_b
-fastboot delete-logical-partition odm_b
